8e5342c5ed08aa4a413fbbdaa50f8a38cd0ae269
asos-craigmorten <craig.morten@asos.com>
2019-06-19 09:12:54 +0100

    feat: housekeeping

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const fs = require("fs");
 const madge = require("madge");
 const git = require("nodegit-kit");
 
-const blacklist = ["node_modules"];
+const blacklist = ["node_modules", "__snapshots__"];
 const whitelist = [".test.js"];
+
+const errors = {
+  invalidDirectory: "provided directory does not exist",
+  emptyBranch: "provided branch must not be empty",
+  git: "unable to retrieve git diff for provided directory and branch",
+  dependencies: "unable to find file depedencies for provided directory"
+};
 
 const help = () =>
   `
@@ -18,18 +25,16 @@ Flags:
 
 const logger = (...msgs) => msgs.map(msg => console.log(msg));
 const exitWithError = (...errors) =>
-  logger(...errors, help()) && process.exit(1);
+  logger("Error", ...errors, help()) && process.exit(1);
 
 const getDirectory = ({ directory, d }) => directory || d;
 const getBranch = ({ branch, b }) => branch || b;
 
 const validateDir = args =>
-  fs.existsSync(getDirectory(args)) ||
-  exitWithError("invalid git directory provided - does not exist");
+  fs.existsSync(getDirectory(args)) || exitWithError(errors.invalidDirectory);
 
 const validateBranch = args =>
-  getBranch(args) ||
-  exitWithError("invalid git branch provided - must be non-empty");
+  getBranch(args) || exitWithError(errors.emptyBranch);
 
 const validateArgs = args => validateDir(args) && validateBranch(args);
 
@@ -44,29 +49,33 @@ const getFileDiff = async (directory, branch) =>
     .then(([repo, commit1, commit2]) =>
       git.diff(repo, commit1, commit2, { "name-only": true })
     )
-    .catch(({ message }) =>
-      exitWithError("unable to retrieve git information", message)
-    );
+    .catch(({ message }) => exitWithError(errors.git, message));
 
 const isBlacklisted = file => blacklist.find(term => file.includes(term));
 const isWhitelisted = file => whitelist.find(term => file.includes(term));
 
+const unique = arr => [...new Set(arr)];
+
 const getFilesToRun = async (directory, filesDiff) =>
   await madge(directory)
     .then(res =>
-      filesDiff
-        .reduce((list, file) => [...list, ...res.depends(file)], [])
-        .filter(file => !isBlacklisted(file) && isWhitelisted(file))
+      unique(
+        filesDiff
+          .reduce((list, file) => [...list, ...res.depends(file)], [])
+          .concat(filesDiff)
+      ).filter(file => !isBlacklisted(file) && isWhitelisted(file))
     )
-    .catch(({ message }) =>
-      exitWithError(
-        "unable to find depedencies within provided directory",
-        message
-      )
-    );
+    .catch(({ message }) => exitWithError(errors.dependencies, message));
 
-module.exports = async () => {
-  const args = minimist(process.argv.slice(2));
+const merge = (cargs, iargs) =>
+  Object.entries(iargs).reduce(
+    (args, [key, value]) =>
+      typeof value !== "undefined" ? { ...args, [key]: value } : args,
+    cargs
+  );
+
+module.exports = async (iargs = {}) => {
+  const args = merge(minimist(process.argv.slice(2)), iargs);
   validateArgs(args);
 
   const directory = getDirectory(args);
@@ -74,4 +83,6 @@ module.exports = async () => {
   const filesDiff = await getFileDiff(directory, branch);
   const filesToRun = await getFilesToRun(directory, filesDiff);
   logger(...filesToRun);
+
+  return filesToRun;
 };
